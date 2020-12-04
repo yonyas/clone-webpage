a80d561d0dff92e679a941e336eb0ca3c1a25ccf
JiyonLee <jiyonlee.d@gmail.com>
2020-12-04 23:00:48 +0900

    second finish

--- script.js
+++
@@ -1,8 +0,0 @@
-const moreBtn = document.querySelector(".fa-sort-up");
-const title = document.querySelector(".info .metadata .title");
-
-moreBtn.addEventListener("click", function () {
-  moreBtn.classList.toggle("clicked");
-  title.classList.toggle("clamp");
-  console.log("hi");
-});
